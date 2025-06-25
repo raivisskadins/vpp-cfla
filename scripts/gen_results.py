86d076b88c3511aaedbcd97c9014a084edce5809
jrudule <jrudule@gmail.com>
2025-06-25 17:09:47 +0300

    update

--- scripts/gen_results.py
+++ scripts/gen_results.py
@@ -1,4 +1,4 @@
-from .utilities import ask_question_save_answer, get_extra_info, get_supplementary_info 
+from .utilities import ask_question_save_answer, get_extra_info
 
 def skip_answer(singlea): 
     return ('answer' in singlea and '?' in singlea['answer']) or ('answer0' in singlea and '?' in singlea['answer0']) 
@@ -14,6 +14,30 @@ def set_extra_info(question_data, pilchapters, mk107chapters, nslchapters, mki3c
    info = qnaengine.compressPrompt(info, 3000) 
    return info 
 
+def add_result(qtype, qnaengine, embedding_conf, promptdict, extrainfo, question_data, answer_data, ofile, results_table):
+    bcontinue_main_question = True
+    if qtype == 'question0':
+        current_prompt = promptdict['0']
+        suffix = '0'
+    else:
+        current_prompt = promptdict[str(question_data['nr'])]
+        suffix = ''
+
+    result = ask_question_save_answer( 
+        qnaengine, embedding_conf, current_prompt + extrainfo, 
+        question_data[qtype], f"{question_data['nr']}-{suffix}", answer_data[f"answer{suffix}"]
+    ) 
+    results_table.append(result) 
+    
+    if result[1] != result[2]:
+        get_singleq_nodes(qnaengine, question_data, qtype, embedding_conf, ofile) 
+    
+    if qtype == 'question0' and result[1] == 'nē': 
+        bcontinue_main_question = False  
+         
+    return results_table, bcontinue_main_question
+    
+
 def _process_single_question(question_data, answer_data, qnaengine, embedding_conf, promptdict, pilchapters, mk107chapters, nslchapters, mki3chapters, ofile, results_table): 
     if 'check' in question_data: 
         return True 
@@ -23,42 +47,24 @@ def _process_single_question(question_data, answer_data, qnaengine, embedding_co
  
     if 'question0' in question_data: 
         if skip_answer(answer_data): 
-            return True
-        
-        result0 = ask_question_save_answer( 
-            qnaengine, embedding_conf, promptdict['0'] + extrainfo, 
-            question_data['question0'], f"{question_data['nr']}-0", answer_data['answer0'] 
-        ) 
-        results_table.append(result0) 
-        
-        if result0[1] != result0[2]:
-            get_singleq_nodes(qnaengine, question_data, 'question0', embedding_conf, ofile) 
-        
-        if result0[1] == 'nē': 
-            bcontinue_main_question = False 
+            return True 
+        results_table, bcontinue_main_question = add_result('question0', qnaengine, embedding_conf, promptdict, extrainfo, question_data, answer_data, ofile, results_table)
     
     if not bcontinue_main_question: 
         q_nr_to_add = str(question_data['nr']) 
         answer_to_add = answer_data.get('answer', answer_data.get('answer0', ''))  
         results_table.append([q_nr_to_add, 'n/a', answer_to_add, '']) 
+
     elif 'question' in question_data: 
         if skip_answer(answer_data): 
-            return False
-            
-        result = ask_question_save_answer( 
-            qnaengine, embedding_conf, promptdict[str(question_data['nr'])] + extrainfo, 
-            question_data['question'], str(question_data['nr']), answer_data['answer'] 
-        ) 
-        results_table.append(result) 
-        
-        if result[1] != result[2]:
-            get_singleq_nodes(qnaengine, question_data, 'question', embedding_conf, ofile) 
+            return False  
+        results_table, bcontinue_main_question = add_result('question', qnaengine, embedding_conf, promptdict, extrainfo, question_data, answer_data, ofile, results_table)
     
     return False 
 
-def gen_results(qnaengine, configfile, embedding_conf, question_dictionary, answer_dictionary, promptdict): 
+def gen_results(qnaengine, configfile, embedding_conf, question_dictionary, answer_dictionary, promptdict, supplementary_info): 
     results_table = []
-    pilchapters, mk107chapters, nslchapters, mki3chapters = get_supplementary_info()  #Jābūt ārpus inifile cikla 
+    pilchapters, mk107chapters, nslchapters, mki3chapters = supplementary_info  
     # Generating output file
     with open("nodes.log", 'a', encoding='utf-8') as ofile: 
         print(f"""\n*********************\n{configfile}, {configfile} 
