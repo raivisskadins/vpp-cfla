71fdad9e8a43ea231567e80d7bfef6c82d08b818
jrudule <jrudule@gmail.com>
2025-07-01 15:45:37 +0300

    added chunk col

--- scripts/gen_results.py
+++ scripts/gen_results.py
@@ -6,7 +6,7 @@ def is_skip_answer(singlea):
 def get_singleq_nodes(qnaengine, question_data, qtype, embedding_conf, ofile): 
     nodes = qnaengine.getSimilarNodes(question_data[qtype], embedding_conf["top_similar"]) 
     q_nr = f"{question_data['nr']}-0" if qtype == 'question0' else question_data['nr']  
-    print(f"\nQ: {q_nr}\n{nodes['text']}\n{nodes['metadata']}\n{nodes['score']}", file=ofile) 
+    print(f"\nQ: {q_nr}\n{nodes['text']}\n{nodes['metadata']}\n{nodes['score']}", file=ofile)
 
 def set_extra_info(question_data, pilchapters, mk107chapters, nslchapters, mki3chapters, qnaengine): 
     print(question_data['nr'], end=' ')
@@ -16,24 +16,29 @@ def set_extra_info(question_data, pilchapters, mk107chapters, nslchapters, mki3c
 
 def add_result(qtype, qnaengine, embedding_conf, promptdict, extrainfo, question_data, answer_data, ofile, results_table):
     bcontinue_main_question = True
+    nodes = qnaengine.getSimilarNodes(question_data[qtype], embedding_conf["top_similar"])
+
     if qtype == 'question0':
         current_prompt = promptdict['0']
-        suffix = '0'
+        suffix = f"{question_data['nr']}-0", answer_data[f"answer0"]
     else:
         current_prompt = promptdict[str(question_data['nr'])]
-        suffix = ''
+        suffix = f"{question_data['nr']}", answer_data[f"answer"]
 
     result = ask_question_save_answer( 
         qnaengine, embedding_conf, current_prompt + extrainfo, 
-        question_data[qtype], f"{question_data['nr']}-{suffix}", answer_data[f"answer{suffix}"]
+        question_data[qtype], {suffix}
     ) 
-    results_table.append(result)
 
     if result[1] != result[2]:
         get_singleq_nodes(qnaengine, question_data, qtype, embedding_conf, ofile) 
+
+    result.append(nodes['text'])
     
     if qtype == 'question0' and result[1] == 'nē': 
         bcontinue_main_question = False  
+
+    results_table.append(result)
          
     return results_table, bcontinue_main_question
     
@@ -48,7 +53,8 @@ def _process_single_question(question_data, answer_data, qnaengine, embedding_co
     if 'question0' in question_data: 
         if is_skip_answer(answer_data): 
             return True 
-        results_table, bcontinue_main_question = add_result('question0', qnaengine, embedding_conf, promptdict, extrainfo, question_data, answer_data, ofile, results_table)
+        else:
+            results_table, bcontinue_main_question = add_result('question0', qnaengine, embedding_conf, promptdict, extrainfo, question_data, answer_data, ofile, results_table)
     
     if not bcontinue_main_question: 
         q_nr_to_add = str(question_data['nr']) 
@@ -58,7 +64,9 @@ def _process_single_question(question_data, answer_data, qnaengine, embedding_co
     elif 'question' in question_data: 
         if is_skip_answer(answer_data): 
             return False  
-        results_table, bcontinue_main_question = add_result('question', qnaengine, embedding_conf, promptdict, extrainfo, question_data, answer_data, ofile, results_table)
+        else:
+            results_table, bcontinue_main_question = add_result('question', qnaengine, embedding_conf, promptdict, extrainfo, question_data, answer_data, ofile, results_table)
+    
     return False 
 
 def gen_results(qnaengine, configfile, embedding_conf, question_dictionary, answer_dictionary, promptdict, supplementary_info): 
