ff4c0c1ed837ddd239c4dfa1e22913294e96babf
Raivis Skadins <raivis.skadins@tilde.com>
2025-07-25 15:58:11 +0300

    small changes

--- scripts/vectorindex.py
+++ scripts/vectorindex.py
@@ -348,8 +348,7 @@ class QnAEngine:
                 newquery = PromptTemplate(
                     query_prompt + "\n" + text_qa_template_str
                 ).format(context_str="\n".join(nodelist), query_str=q)
-                
-                print(newquery)
+
                 result = self.llm.complete(newquery)
                     
                 self.chached_responses[(query_prompt,q)] = str(result)
