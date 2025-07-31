d0c529b64d615bf8c0ab996b835e8a4911cacd7e
Andris Vītols <andrim.vitoam@gmail.com>
2025-07-31 20:24:14 +0300

    updated scripts; added pseudo code

--- web_app/backend/main.py
+++ web_app/backend/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, File, UploadFile
 import shutil
 import os
+from scripts import main_script
 
 app = FastAPI()
 
@@ -10,38 +11,46 @@ os.makedirs(UPLOAD_DIR, exist_ok=True)
 @app.post("/process_procurement")
 async def process_procurement(Proc_ID, procurement_file: UploadFile = File(...), agreement_file: UploadFile = File(...)): 
     
+    # Create new procurement directory
     procurement_dir = UPLOAD_DIR / Proc_ID
     os.makedirs(procurement_dir, exist_ok=True)
 
+    # Upload procurement file
     file_path = os.path.join(procurement_dir, procurement_file.filename)
     with open(file_path, "wb") as buffer:
         shutil.copyfileobj(procurement_file.file, buffer)
 
+    # Upload agreement file
     if agreement_file:
         # Upload agreement file
         return
     
-    # main script (pass in, proc_id, procurement_file, agreement_file, other data) -> generates csv file
-
-    return {"proc_path": procurement_dir} # Get procurment_file path
+    # main_script(procurement_file, agreement_file) # this generates a csv file; Make sure it's in the same procurement directory; 
+    
+    return {"proc_report_path": procurement_dir / "report.csv"} # Get procurement csv file path 
 
 @app.get("/get_csv_info")
-async def get_csv_info(procurement_file_name):
-    #with open(procurement_file_name, "wb") as buffer:
-    df = pd.read_csv(procurement_file_name, keep_default_na=False, encoding="utf-8")
-    # turn csv -> json list of objects
-    # when extracting Pamatojums, get only the explanation key
-    #     {
-    #   ""answer"": ""jā"",
-    #   ""rate"": ""augsta"",
-    #   ""explanation"": ""Kontekstā ir norādīts, ka iepirkuma priekšmets nav sadalīts daļās. Iepirkuma priekšmets netiek dalīts daļās, jo ir viens pretendentu loks; viens būvobjekts; viens būvdarbu veikšanas laiks būvobjektā.""
-    # }
-    procurement_data = df.json
-    return procurement_data
+async def get_csv_info(proc_report_path):
+    
+    # Read in the report csv file
+
+    # df = pd.read_csv(proc_report_path, keep_default_na=False, encoding="utf-8")
+    
+    # turn csv into json list of objects
+
     # List of objects like these:
     #     {
     #     "Nr": "8",
     #     "Atbilde": "nē",
     #     "Pamatojums": "Nezinu"
     # }
+
+    # when extracting Pamatojums, get only the explanation key
+    #     {
+    #   ""answer"": ""jā"",
+    #   ""rate"": ""augsta"",
+    #   ""explanation"": ""Kontekstā ir norādīts, ka iepirkuma priekšmets nav sadalīts daļās. Iepirkuma priekšmets netiek dalīts daļās, jo ir viens pretendentu loks; viens būvobjekts; viens būvdarbu veikšanas laiks būvobjektā.""
+    # }
     
+    # procurement_data = df.json
+    # return procurement_data
